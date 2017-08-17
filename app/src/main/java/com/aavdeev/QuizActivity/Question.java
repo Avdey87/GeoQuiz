20449c414f45c83124abd5cc4029c3b48b2899bd
avdey <avdey87@gmail.com>
2017-08-17 16:49:38 +0300

    Initial commit

---
+++ app/src/main/java/com/aavdeev/QuizActivity/Question.java
@@ -0,0 +1,42 @@
+package com.aavdeev.QuizActivity;
+
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+
+    public int getTextResID() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
