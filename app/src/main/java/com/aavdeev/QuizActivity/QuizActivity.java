de40400d4d929b17cd1b74a8b6335c2f857a435f
avdey <avdey87@gmail.com>
2017-09-06 13:45:12 +0300

    page 131 Chapter 5

--- app/src/main/java/com/aavdeev/QuizActivity/QuizActivity.java
+++ app/src/main/java/com/aavdeev/QuizActivity/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.aavdeev.QuizActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,9 +21,11 @@ public class QuizActivity extends AppCompatActivity {
     private Button mCheatButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
+    private boolean mIsCheater;
     private TextView mQuestionTextView;
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
 
     private Question[] mQuestionBank = new Question[]{
@@ -44,10 +47,14 @@ public class QuizActivity extends AppCompatActivity {
 
         int messageResId = 0;
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+     if (mIsCheater){
+            messageResId = R.string.judgement_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+         if (userPressedTrue == answerIsTrue) {
+             messageResId = R.string.correct_toast;
+         } else {
+             messageResId = R.string.incorrect_toast;
+         }
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
@@ -77,6 +84,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
@@ -105,16 +113,6 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-        mCheatButton = (Button) findViewById(R.id.cheat_button);
-        mCheatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
-                startActivity(i);
-            }
-        });
-
 
         mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
@@ -130,10 +128,33 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(i, REQUEST_CODE_CHEAT);
+
+            }
+        });
+
 
         updateQuestion();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerButton(data);
+        }
+    }
 
     @Override
     protected void onStart() {
