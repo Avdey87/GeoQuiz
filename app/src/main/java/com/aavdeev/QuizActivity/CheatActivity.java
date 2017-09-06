667c0f596507a990c5d0efb8f1b36c80468ad4ed
avdey <avdey87@gmail.com>
2017-09-06 17:33:04 +0300

    page 139 Chapter 6

--- app/src/main/java/com/aavdeev/QuizActivity/CheatActivity.java
+++ app/src/main/java/com/aavdeev/QuizActivity/CheatActivity.java
@@ -1,32 +1,43 @@
 package com.aavdeev.QuizActivity;
 
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 public class CheatActivity extends AppCompatActivity {
 
-    public static final String EXTRA_ANSWER_IS_TRUE =
-            "com.aavdeev.QuizActivity.answer_is_true";
+    public static final String EXTRA_ANSWER_IS_TRUE ="com.aavdeev.QuizActivity.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOW ="com.aavdeev.QuizActivity.answer_show";
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private boolean answerShownResult;
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
-
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
-
-        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
-        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+        mAnswerTextView = (TextView)findViewById(R.id.answer_text_view);
+        mShowAnswer = (Button)findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,15 +45,45 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.true_button);
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
-
+                }
+                setAnswerShownResult(true);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
                 }
             }
-        });
+        });}
+
+    private void setAnnswerShowResult(boolean isAnswerShow) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOW, isAnswerShow);
+        setResult(RESULT_OK, data);
+
     }
 
-    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
-        Intent i = new Intent(packageContext, CheatActivity.class);
-        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
-        return i;
+    public void setAnswerShownResult(boolean answerShownResult) {
+        this.answerShownResult = answerShownResult;
+    }
+
+    public static boolean wasAnswerButton(Intent data) {
+        return false;
     }
 }
+
+
+
